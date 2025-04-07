9358c875b70c0f1e057c673ea235d12ffa2c52b2
Jayden-digipen <1494644@edu.burnabyschools.ca>
2025-04-07 11:42:17 -0700

    Jayden
    1. Sprinting done
    2. Stamina almost done
    3. Crouching (home)

--- Assets/Jayden/PlayerMovement.cs
+++ Assets/Jayden/PlayerMovement.cs
@@ -1,11 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayerMovement : MonoBehaviour
 {
+    StaminaUI staminaUI;
     [SerializeField] Transform playerCapsule;
-    public float movementSpeed;
+    
+    [SerializeField] private float currentSpeed = 4.5f;
+    public float walkingSpeed = 4.5f;
+    public float runningSpeed = 10f;
+
+
+    [SerializeField] private float currentAccelerator = 5f;
+    public KeyCode sprintKey = KeyCode.LeftShift;
+
+    [SerializeField] private float Stamina = 100f;
+    [SerializeField] private float StaminaDecreaser = 1f;
+    [SerializeField] private float StaminaIncreaser = 0.2f;
+    private float staminaCountdown = 20;
+    
 
     public Transform orientationPlayerCameraDirection;
     Transform playerTransform;
@@ -17,14 +34,17 @@ public class PlayerMovement : MonoBehaviour
 
     Rigidbody rb;
 
-    [SerializeField] Vector3 velocityCheck;
-
     
 
+    public MovementState state;
+    public enum MovementState
+    {
+        walking,
+        running
+    }
 
     private void Start()
-    {
-       
+    {      
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
     }
@@ -33,11 +53,15 @@ public class PlayerMovement : MonoBehaviour
     {
         playerCapsule.transform.rotation = orientationPlayerCameraDirection.transform.rotation;
         MyInput();
-        SpeedControl();
+        WalkingRunning();
+        StateHandler();
+       
     }
 
+   
+
     private void FixedUpdate()
-    {
+    {    
         MovePlayer();
     }
 
@@ -47,22 +71,53 @@ public class PlayerMovement : MonoBehaviour
         verticalInput = Input.GetAxisRaw("Vertical");
     }
 
-    private void MovePlayer()
+    private void StateHandler()
     {
-       
-        moveDirection = orientationPlayerCameraDirection.forward * verticalInput + orientationPlayerCameraDirection.right * horizontalInput;
-        rb.AddForce(moveDirection.normalized * movementSpeed * 4f, ForceMode.Force);
-    }
-
-    private void SpeedControl()
-    {
-        Vector3 flatVelocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
-        velocityCheck = flatVelocity;
-        
-        if (flatVelocity.magnitude > movementSpeed)
+         
+        if (Input.GetKey(sprintKey) & Stamina > 0 & staminaCountdown <= 0)
         {
-            Vector3 limitedVelocity = flatVelocity.normalized * movementSpeed;
-            rb.velocity = new Vector3(limitedVelocity.x, rb.velocity.y, limitedVelocity.z);
+            state = MovementState.running;
+            currentSpeed = runningSpeed;
+            staminaCountdown = 20f;
+            Stamina -= StaminaDecreaser;
+            staminaUI.StaminaBar.fillAmount = Stamina/100;
+
+        }
+
+        else
+        {
+            state = MovementState.walking;
+            currentSpeed = walkingSpeed;
+            staminaCountdown -= Time.deltaTime;
+            
+            if(Stamina <= 100)
+            {
+                Stamina += StaminaIncreaser;
+            }
+            staminaUI.StaminaBar.fillAmount = Stamina/100;
         }
     }
+
+
+
+    private void MovePlayer()
+    {    
+        moveDirection = orientationPlayerCameraDirection.forward * verticalInput + orientationPlayerCameraDirection.right * horizontalInput;
+        rb.AddForce(moveDirection.normalized * currentSpeed * currentAccelerator, ForceMode.Force);
+    }
+
+    private void WalkingRunning()
+    {
+        Vector3 flatVelocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        Debug.Log(flatVelocity.magnitude);
+        
+        if (flatVelocity.magnitude >= currentSpeed)
+        {
+            Vector3 limitedVelocity = flatVelocity.normalized * walkingSpeed;
+            rb.velocity = new Vector3(limitedVelocity.x, rb.velocity.y, limitedVelocity.z);
+        }        
+    }
+
+   
+   
 }
