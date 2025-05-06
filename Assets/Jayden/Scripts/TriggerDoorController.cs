a997af0cdaf12eb2cbca01dd78439fd1dec8d99e
Jayden-digipen <1494644@edu.burnabyschools.ca>
2025-05-06 11:46:07 -0700

    Door

--- Assets/Jayden/Scripts/TriggerDoorController.cs
+++ Assets/Jayden/Scripts/TriggerDoorController.cs
@@ -4,9 +4,36 @@ using UnityEngine;
 
 public class TriggerDoorController : MonoBehaviour
 {
-    [SerializeField] private Animator myDoor = null;
+    [SerializeField] private Animator myDoor;
     [SerializeField] private bool openTrigger = false;
     [SerializeField] private bool closeTrigger = false;
+    [SerializeField] GameObject textOpen;
+    [SerializeField] GameObject textClose;
+    public bool inOpenTrigger = false;
+    public bool inCloseTrigger = false;
+
+    private void Start()
+    {
+        textOpen.SetActive(false);
+        textClose.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (inOpenTrigger)
+            {
+                myDoor.Play("DoorOpen", 0, 0.0f);
+            }
+
+            else if (inCloseTrigger)
+            {
+                myDoor.Play("DoorClose", 0, 0.0f);
+            }
+        }
+    }
+
 
     private void OnTriggerEnter(Collider other)
     {
@@ -14,14 +41,36 @@ public class TriggerDoorController : MonoBehaviour
         {
             if (openTrigger)
             {
-                myDoor.Play("DoorOpen", 0, 0.0f);
-                gameObject.SetActive(false);
+                textOpen.SetActive(true);
+                inOpenTrigger = true;
+                          
             }
 
             else if (closeTrigger)
             {
-                myDoor.Play("DoorClose", 0, 0.0f);
-                gameObject.SetActive(false);
+                textClose.SetActive(true);
+                inCloseTrigger = true;
+   
+            }
+        }
+    }
+
+   
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (openTrigger)
+            {
+                textOpen.SetActive(false);
+                inOpenTrigger = true;
+            }
+
+            else if (closeTrigger)
+            {
+                textClose.SetActive(false);
+                inCloseTrigger = true;
             }
         }
     }
