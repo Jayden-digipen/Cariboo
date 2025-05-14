84d7c0e50425fc8304e3c6bffd56c66f93de0fea
Jayden-digipen <1494644@edu.burnabyschools.ca>
2025-05-13 23:02:00 -0700

    door fixed

--- Assets/Jayden/Scripts/TriggerDoorController.cs
+++ Assets/Jayden/Scripts/TriggerDoorController.cs
@@ -1,104 +1,127 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System.Threading;
 
 public class TriggerDoorController : MonoBehaviour
 {
-    DoorCrossCheck doorCrossCheck;
-    float timeDuration = 1;
-    [SerializeField] private Animator myDoor;
-    [SerializeField] private bool inTrigger = false;
-    [SerializeField] bool doorIsOpen = false;
-    [SerializeField] TextMeshPro textOpen;
-    [SerializeField] TextMeshPro textClose;
-    bool inOpenTrigger = false;
-    bool inCloseTrigger = false;
-    bool hasCrossedDoor;
+    static TriggerDoorController activeDoor; //  active once at a time
+    [SerializeField] GameObject doorTrigger;
     
+
+    [SerializeField] private Animator myDoor;
+    [SerializeField] private TextMeshPro textOpen;
+    [SerializeField] private TextMeshPro textClose;
 
     [SerializeField] private string doorOpen = "DoorOpen";
     [SerializeField] private string doorClose = "DoorClose";
 
+    [SerializeField] float timer = 1;
+
+    private bool doorIsOpen = false;
+    bool isInteracting = false;
+    
+
+
     private void Start()
     {
-        doorCrossCheck = FindObjectOfType<DoorCrossCheck>();
-        textOpen.text = "";
-        textClose.text = "";
+        
     }
-
     private void Update()
     {
-        hasCrossedDoor = doorCrossCheck.hasWalkedThroughDoor;
+        timer = Mathf.Clamp(timer, 0f, 1f);
 
-        if (Input.GetKeyDown(KeyCode.E))
+        
+
+        if (activeDoor == this && Input.GetKeyDown(KeyCode.E) && !isInteracting)
         {
-            if (inTrigger && !doorIsOpen)
-            {
-                myDoor.Play(doorOpen, 0, 0.0f);
-                doorIsOpen = true;
-            }
-
-            else if (inTrigger && doorIsOpen)
-            {
-                myDoor.Play(doorClose, 0, 0.0f);
-                doorIsOpen = false;          
-            }
-
-            
+            StartCoroutine(Countdown());
         }
     }
 
-
-    public void OnTriggerEnter(Collider other)
+    IEnumerator Countdown()
     {
-        
+        isInteracting = true;
+        ToggleDoor();
+        yield return new WaitForSeconds(timer);
+        isInteracting = false;
+
+    }
+
+    public void ToggleDoor()
+    {
+        if (!doorIsOpen)
+        {
+            myDoor.Play(doorOpen, 0, 0.0f);
+            
+            doorIsOpen = true;
+            
+        }
+        else
+        {
+            myDoor.Play(doorClose, 0, 0.0f);
+           
+            doorIsOpen = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
         if (other.CompareTag("Player"))
         {
-            inTrigger = true;
-            
-            if (!doorIsOpen && !hasCrossedDoor)
-            {
-                textOpen.text = "E to open";
-                    
-            }
+            activeDoor = this;
 
-            else if (!doorIsOpen && hasCrossedDoor)
-            {
-                textClose.text = "E to open";
-
-            }
-
-
-            else if (doorIsOpen && hasCrossedDoor)
-            {
-                
-                textClose.text = "E to close";
-            }
-
-            else
-            {
-                textOpen.text = "E to close";
-            }
-                
-                          
-            
-
-            
+          
         }
     }
 
-   
+    private void OnTriggerStay(Collider other)
+    {
+       bool hasCrossedDoor = doorTrigger.GetComponent<DoorCrossCheck>().hasWalkedThroughDoor;
+
+        if (!doorIsOpen && !hasCrossedDoor)
+        {
+            textOpen.text = "E to interact";
+            textClose.text = "";
+            Debug.Log("1");
+        }
+
+
+        else if (doorIsOpen && hasCrossedDoor)
+        {
+            textClose.text = "E to interact";
+            textOpen.text = "";
+            Debug.Log("2");
+
+
+        }
+
+        else if (doorIsOpen && !hasCrossedDoor)
+        {
+
+            textOpen.text = "E to interact";
+            textClose.text = "";
+            Debug.Log("3");
+
+        }
+
+        else
+        {
+            textClose.text = "E to interact";
+            textOpen.text = "";
+            Debug.Log("4");
+        }
+    }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Player"))
-        {           
+        if (other.CompareTag("Player") && activeDoor == this)
+        {
             textOpen.text = "";
             textClose.text = "";
-            inTrigger = false;
-              
-                      
+            activeDoor = null;
         }
     }
 }
