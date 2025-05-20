75b645f6ed864a2914a961ed236412e41d9dba3e
Jayden-digipen <1494644@edu.burnabyschools.ca>
2025-05-20 10:09:05 -0700

    material rotate

--- Assets/Jayden/Scripts/Key.cs
+++ Assets/Jayden/Scripts/Key.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Key : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] float concealmentTime = 10;
+    [SerializeField] GameObject player;
+    Item item;
+    InventoryManager inventoryManager;
+    float imageShower;
+
+    private void Start()
     {
         
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        GetSelectedItem();
     }
+
+    private void GetSelectedItem()
+    {
+        if ((InventoryManager.instance != null && InventoryManager.instance.selectedSlot != -1))
+        {
+            Item item = InventoryManager.instance.GetSelectedItem(false);
+
+            if (item != null && item.type == ItemType.Keys)
+            {
+                InventoryManager.instance.GetSelectedItem(true);
+                
+
+
+               
+               
+
+            }
+
+
+        }
+
+
+
+    }
+
+   
 }
